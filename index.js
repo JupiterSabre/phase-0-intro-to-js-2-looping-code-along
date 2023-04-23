7503b2c75d30faed9a61fa5fc20339d3ea46115e
Zulai Romero <zulairomero@gmail.com>
2023-04-23 14:15:53 -0400

    adds completed labs for FOR and WHILE loops

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+// Thank you messages assignment
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "suprise";
+
+function writeCards(names, event) {
+  const tyMessage = [];
+
+  for (let i = 0; i < names.length; i++) {
+    tyMessage.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  } 
+  return tyMessage;
+}
+
+console.log(writeCards(names, "surprise"));
+
+// Countdown Assignment
+function countDown(number) {
+  while (number >=  0) {
+    console.log(number--);
+  }
+   }
+   console.log(countDown(35));
+
+ 
+
+
